3106ea91b0836f8d63cca4a8f60346afdb89619c
Daniel Sanchez <atlow.archive@gmail.com>
2024-05-19 19:20:20 -0700

    fix some spacing issues

--- app/page.tsx
+++ app/page.tsx
@@ -16,22 +16,22 @@ export default function Home() {
           {/* Content Container */}
           <div className="mb-12 pt-12 xl:mb-0 xl:w-1/2 xl:pt-0">
             {/* Headline */}
-            <h1 className="mb-4 text-4xl font-semibold text-neutral-900 md:text-5xl xl:text-6xl">
+            <h1 className="mb-4 text-4xl font-semibold text-neutral-900 md:mb-6 md:text-5xl xl:text-6xl">
               Well crafted abstract images
             </h1>
             {/* Description  */}
-            <p className="mb-8 text-lg font-normal text-neutral-600 md:text-xl">
+            <p className="mb-8 text-lg font-normal text-neutral-600 md:mb-16 md:text-xl">
               High quality abstract images for your projects, wallpaper and presentations.
             </p>
             {/* CTA Container */}
             <div className="flex space-x-4 md:w-[458px] md:space-x-8 xl:w-9/12">
               {/* Button Learn More */}
               {/* TODO: create a component that takes a button type and to set this styling once and adjust with types */}
-              <button className="w-full rounded border border-neutral-200 bg-white px-[29px] py-3 text-base font-medium text-neutral-900 shadow hover:bg-neutral-50 focus-visible:rounded-md focus-visible:bg-neutral-50 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-indigo-600/[.12] disabled:border-none disabled:bg-neutral-100 disabled:text-neutral-400 disabled:shadow-none md:px-14 xl:px-9">
+              <button className="w-full rounded border border-neutral-200 bg-white px-[29px] py-3 text-base font-medium text-neutral-900 shadow hover:bg-neutral-50 focus-visible:rounded-md focus-visible:bg-neutral-50 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-indigo-600/[.12] disabled:border-none disabled:bg-neutral-100 disabled:text-neutral-400 disabled:shadow-none md:px-14 md:py-4 xl:px-9">
                 Learn more
               </button>
               {/* Button See Pricing */}
-              <button className="w-full rounded bg-indigo-700 px-[29px] py-3 text-base font-medium text-white shadow hover:bg-indigo-800 focus-visible:rounded-md focus-visible:bg-indigo-800 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-indigo-600/[.12] disabled:border-none disabled:bg-neutral-100 disabled:text-neutral-400 disabled:shadow-none md:px-14 xl:px-9">
+              <button className="w-full rounded bg-indigo-700 px-[29px] py-3 text-base font-medium text-white shadow hover:bg-indigo-800 focus-visible:rounded-md focus-visible:bg-indigo-800 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-indigo-600/[.12] disabled:border-none disabled:bg-neutral-100 disabled:text-neutral-400 disabled:shadow-none md:px-14 md:py-4 xl:px-9">
                 See pricing
               </button>
             </div>
